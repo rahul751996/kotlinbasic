649b20f74fc4c21d094da04afafdc86096233cb5
Rahul verma <rahul.kumar.cs.2014@miet.ac.in>
2017-06-23 13:55:46 +0530

    kotlin basic

---
+++ app/src/main/java/com/example/coolbuddie/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.coolbuddie.myapplication
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+
